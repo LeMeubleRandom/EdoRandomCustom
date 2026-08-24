3d936620a6c6694c9606a61445c6236d18242b08
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-08-24 23:31:15 +0200

    first script test

--- script/c987000001.lua
+++ script/c987000001.lua
@@ -1,1 +1,48 @@
+--Hieratic Dragon of Seth
 local s,id=GetID()
+function s.initial_effect(c)
+    --Special Summon Procedure
+    local e0=Effect.CreateEffect(c)
+    e0:SetDescription(aux.Stringid(id,0))
+    e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetRange(LOCATION_HAND)
+    e0:SetCountLimit(1,{id,0},EFFECT_COUNT_CODE_OATH)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+
+	e0:SetCondition(s.hspcon)
+	e0:SetTarget(s.hsptg)
+	e0:SetOperation(s.hspop)
+
+	c:RegisterEffect(e0)
+end
+s.listed_series={SET_HIERATIC}
+
+function s.searchfilter(c)
+	return c:IsSetCard(SET_HIERATIC)
+end
+
+function s.hspcon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    local rg=Duel.GetMatchingGroup(Card.IsReleasable,tp,LOCATION_HAND,LOCATION_MZONE,c,tp)
+    return #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
+end
+
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,c)
+    local rg=Duel.GetMatchingGroup(Card.IsReleasable,tp,LOCATION_HAND,LOCATION_MZONE,c,tp)
+    local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_CONFIRM,nil,nil,true)
+    if g and #g>0 then
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+    local sg=e:GetLabelObject()
+		if sg and #sg>0 then
+			c:SetMaterial(sg)
+			Duel.Release(sg,REASON_SUMMON|REASON_MATERIAL)
+		end
+end
