b4b8136061bc7dd02bd4cff05421044e2b700f4b
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-08-25 10:38:00 +0200

    script test

--- script/c987000001.lua
+++ script/c987000001.lua
@@ -3,32 +3,40 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Special Summon Procedure
     local e0=Effect.CreateEffect(c)
-    e0:SetDescription(aux.Stringid(id,0))
     e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e0:SetType(EFFECT_TYPE_FIELD)
 	e0:SetRange(LOCATION_HAND)
     e0:SetCountLimit(1,{id,0},EFFECT_COUNT_CODE_OATH)
 	e0:SetCode(EFFECT_SPSUMMON_PROC)
-
 	e0:SetCondition(s.hspcon)
 	e0:SetTarget(s.hsptg)
 	e0:SetOperation(s.hspop)
-
 	c:RegisterEffect(e0)
+    --Search Cards
+    local e1a=Effect.CreateEffect(c)
+    e1a:SetDescription(aux.Stringid(id,0))
+    e1a:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_HANDES)
+    e1a:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1a:SetProperty(EFFECT_FLAG_DELAY)
+	e1a:SetCode(EVENT_SUMMON_SUCCESS)
+	e1a:SetCountLimit(1,id)
+	e1a:SetTarget(s.thtg)
+	e1a:SetOperation(s.thop)
+	c:RegisterEffect(e1a)
+    local e1b=e1a:Clone()
+    e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e1b)
 end
 s.listed_series={SET_HIERATIC}
-
 function s.searchfilter(c)
-	return c:IsSetCard(SET_HIERATIC)
+	return c:IsSetCard(SET_HIERATIC) and c:IsAbleToHand()
 end
-
 function s.hspcon(e,c)
     if c==nil then return true end
     local tp=c:GetControler()
     local rg=Duel.GetMatchingGroup(Card.IsReleasable,tp,LOCATION_HAND,LOCATION_MZONE,c,tp)
     return #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
 end
-
 function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
     local rg=Duel.GetMatchingGroup(Card.IsReleasable,tp,LOCATION_HAND|LOCATION_MZONE,0,c,tp)
     local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_RELEASE,nil,nil,true)
@@ -38,10 +46,25 @@ function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
 	end
 	return false
 end
-
 function s.hspop(e,tp,eg,ep,ev,re,r,rp,chk,c)
     local sg=e:GetLabelObject()
 	if sg and #sg>0 then
 		Duel.Release(sg,REASON_COST)
+	end
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.searchfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.searchfilter,tp,LOCATION_DECK,0,nil)
+	if #g==0 then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,2,aux.dncheck,1,tp,HINTMSG_ATOHAND)
+	if #sg>0 and Duel.SendtoHand(sg,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.ShuffleHand(tp)
+		Duel.BreakEffect()
+		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_EFFECT|REASON_DISCARD,nil,REASON_EFFECT)
 	end
 end
