13f805ee1c1ade8eaa94278931dbb1f2db7a4b9c
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-08 00:56:56 +0200

    correct script 03

--- script/Reclaimer/c955000003.lua
+++ script/Reclaimer/c955000003.lua
@@ -75,11 +75,6 @@ function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	if (op and op==1) or (link_chk and b1 and (not b2 or Duel.SelectYesNo(tp,aux.Stringid(id,2)))) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 		local g=Duel.SelectMatchingCard(tp,s.stfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil)
-		if #g>0 then
-			Duel.SSet(tp,g)
-			b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-				and Duel.IsExistingMatchingCard(s.tdfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
-		end
 		breakeffect=true
 	end
 	if (op and op==2) or (link_chk and b2 and (not breakeffect or Duel.SelectYesNo(tp,aux.Stringid(id,3)))) then
