4d6d133db1493c43e34310caf12aa5a11a050f11
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-08 01:16:14 +0200

    correct script 03

--- script/Reclaimer/c955000003.lua
+++ script/Reclaimer/c955000003.lua
@@ -63,7 +63,7 @@ function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local b1=Duel.IsExistingMatchingCard(s.stfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil)
-	local b2=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE,0,nil)
+	local b2=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE,0,nil)>=3
 	local link_chk=Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,CARD_THE_CHOSEN_ONE,CARD_THE_FALLEN_ONE)
 	local op=nil
 	if not link_chk then
