f0b95ef4f313eb76d1adf337a07bbb9f154cc28f
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-07 20:34:52 +0200

    add script 03

---
+++ script/Reclaimer/c955000003.lua
@@ -0,0 +1,37 @@
+Duel.LoadScript("MeubleConstant.lua")
+
+--"I Need A Weapon"
+local s,id=GetID()
+function s.initial_effect(c)
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.acttg)
+	c:RegisterEffect(e1)
+end
+function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if s.sptg(e,tp,eg,ep,ev,re,r,rp,0)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		s.sptg(e,tp,eg,ep,ev,re,r,rp,1)
+		e:SetOperation(s.spop)
+	else
+		e:SetCategory(0)
+		e:SetOperation(nil)
+	end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,Card.IsCanBeSpecialSummoned,tp,LOCATION_GRAVE,0,1,1,nil,e,0,tp,false,false)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(Card.IsCanBeSpecialSummoned,tp,LOCATION_GRAVE,0,1,nil,e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
