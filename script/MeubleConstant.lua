ba7dfce80c012b1be410afcd61371a9766fce46e
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-07 09:11:20 +0200

    correct setcode

--- script/MeubleConstant.lua
+++ script/MeubleConstant.lua
@@ -3,6 +3,8 @@
     Only available with custom cards from this pack (for exemple, D/D Recruits is not treated as a "Recruit" card)
 --]]
 
+--Archetype code
+
 SET_RECRUIT                      = 0x9554
 -- 38228
 SET_INFESTED                     = 0x9555
@@ -11,3 +13,8 @@ SET_ELITE                        = 0x9556
 -- 38230
 SET_SPARTAN                      = 0x9557
 -- 38231
+
+--CardName code
+
+CARD_THE_CHOSEN_ONE              = 955000001
+CARD_THE_FALLEN_ONE              = 955000002
--- script/Reclaimer/c955000001.lua
+++ script/Reclaimer/c955000001.lua
@@ -1,3 +1,5 @@
+Duel.LoadScript("MeubleConstant.lua")
+
 --The Chosen One
 local s,id=GetID()
 function s.initial_effect(c)
@@ -32,13 +34,13 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 function s.matfilter(c,scard,sumtype,tp)
-    return c:IsSetCard(SET_RECRUIT,scard,sumtype,tp) and c:isCode(955000002)
+    return c:IsSetCard(SET_RECRUIT,scard,sumtype,tp) and c:isCode(CARD_THE_FALLEN_ONE)
 end
 function s.dthfilter(c)
 	return c:IsSetCard(SET_RECRUIT) and c:IsSpellTrap() and c:IsAbleToHand()
 end
 function s.cfilter(c)
-	return c:isCode(955000002) and c:IsAbleToGraveAsCost()
+	return c:isCode(CARD_THE_FALLEN_ONE) and c:IsAbleToGraveAsCost()
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
