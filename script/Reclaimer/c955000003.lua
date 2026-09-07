2c20ca450e6d0086c4c9942648704fca6f89560e
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-08 00:27:02 +0200

    correct script 03

--- script/Reclaimer/c955000003.lua
+++ script/Reclaimer/c955000003.lua
@@ -62,9 +62,9 @@ function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SetPossibleOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
-	local b1=Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.stfilter),tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil)
+	local b1=Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.stfilter),tp,LOCATION_DECK,0,1,nil)
 	local b2=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE,0,nil)
-	local link_chk=Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,CARD_THE_CHOSEN_ONE)
+	local link_chk=Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_MZONE,0,1,nil,CARD_THE_CHOSEN_ONE+CARD_THE_FALLEN_ONE)
 	local op=nil
 	if not link_chk then
 		op=Duel.SelectEffect(tp,
