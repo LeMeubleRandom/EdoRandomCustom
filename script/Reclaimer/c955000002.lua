a77e7143766e5225875243be95896fb9f137a485
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-07 09:48:31 +0200

    correction script

--- script/Reclaimer/c955000002.lua
+++ script/Reclaimer/c955000002.lua
@@ -78,5 +78,6 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then return true end
+	c:IsRelateToEffect(e)
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 end
