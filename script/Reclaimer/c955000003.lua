9a1d79ca1db9b05677e6e088d4ad11aad6fc8a4e
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-07 23:39:30 +0200

    correct script 03

--- script/Reclaimer/c955000003.lua
+++ script/Reclaimer/c955000003.lua
@@ -10,7 +10,7 @@ function s.initial_effect(c)
 	e1:SetTarget(s.acttg)
 	c:RegisterEffect(e1)
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,3))
+    e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SET+CATEGORY_TODECK+CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -20,6 +20,7 @@ function s.initial_effect(c)
 	e2:SetOperation(s.effop)
 	c:RegisterEffect(e2)
 end
+--[[
 function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	if s.sptg(e,tp,eg,ep,ev,re,r,rp,0)
@@ -45,19 +46,24 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 		and Duel.IsExistingMatchingCard(Card.IsCanBeSpecialSummoned,tp,LOCATION_GRAVE,0,1,nil,e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
-
-
+]]--
+function s.stfilter(c)
+	return c:IsSetCard(SET_RECRUIT) and c:IsTrap() and c:IsSSetable()
+end
+function s.tdfilter(c,e,tp)
+	return c:IsAbleToDeck()
+end
 function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
-		local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
-		return b1 or b2
+		local b1=Duel.IsExistingMatchingCard(s.stfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,nil)
+		local b2=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE,0,nil)
+		return b1 or b2 and #b2>=3 and b2:IsExists(Card.IsSetCard,1,nil,SET_RECRUIT) and Duel.IsPlayerCanDraw(tp,1)
 	end
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SET,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TODECK|CATEGORY_DRAW,nil,1,tp,LOCATION_GRAVE)
 end
+
+
 function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local b1=Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,nil)
 	local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -66,8 +72,8 @@ function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	local op=nil
 	if not ravine_chk then
 		op=Duel.SelectEffect(tp,
-			{b1,aux.Stringid(id,1)},
-			{b2,aux.Stringid(id,2)})
+			{b1,aux.Stringid(id,2)},
+			{b2,aux.Stringid(id,3)})
 	end
 	local breakeffect=false
 	if (op and op==1) or (ravine_chk and b1 and (not b2 or Duel.SelectYesNo(tp,aux.Stringid(id,3)))) then
