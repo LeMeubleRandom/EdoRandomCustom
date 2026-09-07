0337bcfcd21632c265a79ff139b3592e95db7886
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-07 03:37:52 +0200

    add halo cdb and halo pics

---
+++ script/MeubleConstant.lua
@@ -0,0 +1,13 @@
+--[[
+    Archetype setcode constants list
+    Only available with custom cards from this pack (for exemple, D/D Recruits is not treated as a "Recruit" card)
+--]]
+
+SET_RECRUIT                      = 0x95594
+-- 611732
+SET_INFESTED                     = 0x95595
+-- 611733
+SET_ELITE                        = 0x95596
+-- 611734
+SET_SPARTAN                      = 0x95597
+-- 611735
