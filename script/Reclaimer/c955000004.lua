f837dfe7ade2085a710e48e4ee56fb49790a2a74
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-08 03:05:09 +0200

    correct script 03

---
+++ script/Reclaimer/c955000004.lua
@@ -0,0 +1,39 @@
+Duel.LoadScript("MeubleConstant.lua")
+
+--Earth recruits
+local s,id=GetID()
+function s.initial_effect(c)
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+    local e2=e1:clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
+    local e3=Effect.CreateEffect(c)
+end
+function s.thfilter(c)
+	return c:IsMonster() and c:IsSetCard(SET_RECRUIT) and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned())
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if not tc then return end
+	aux.ToHandOrElse(tc,tp,
+		Card.IsSSetable,
+		function(c)
+			Duel.SSet(tp,tc)
+		end,
+		aux.Stringid(id,0)
+	)
+end
