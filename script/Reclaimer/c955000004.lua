c0cc8d780d3560788401839fb070082c0f84c222
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-08 03:26:18 +0200

    correction script 04

--- script/Reclaimer/c955000004.lua
+++ script/Reclaimer/c955000004.lua
@@ -28,16 +28,24 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local mzone_chk=Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,4))
 	local sc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,1,1,nil,e,tp,mzone_chk):GetFirst()
 	aux.ToHandOrElse(sc,tp,
 		function()
 			return mzone_chk and sc:IsCanBeSpecialSummoned(e,0,tp,false,false)
 		end,
 		function()
-			Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+			if Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP) and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+                local e2=Effect.CreateEffect(c)
+                e2:SetType(EFFECT_TYPE_SINGLE)
+                e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+                e2:SetCode(EFFECT_CHANGE_LEVEL)
+                e2:SetValue(4)
+                e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+                sc:RegisterEffect(e2)
+            end
 		end,
-		aux.Stringid(id,0)
+        Duel.SpecialSummonComplete()
 	)
 	local c=e:GetHandler()
 end
