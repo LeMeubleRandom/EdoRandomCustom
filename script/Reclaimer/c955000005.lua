b9d2fe1974bf4eac20dd4f20784b447407b49bb2
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-09 04:36:37 +0200

    correction script 05

--- script/Reclaimer/c955000005.lua
+++ script/Reclaimer/c955000005.lua
@@ -22,9 +22,9 @@ end
 function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.ssfilter(chkc,e,tp,Duel.GetLocationCount(tp,LOCATION_MZONE)>0) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.ssfilter,tp,LOCATION_GRAVE,1,nil,e,tpDuel.GetLocationCount(tp,LOCATION_MZONE)>0) end
+		and Duel.IsExistingTarget(s.ssfilter,tp,LOCATION_GRAVE,1,nil,e,tp,Duel.GetLocationCount(tp,LOCATION_MZONE)>0) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.ssfilter,tp,LOCATION_GRAVE,1,1,nil,e,tpDuel.GetLocationCount(tp,LOCATION_MZONE)>0)
+	local g=Duel.SelectTarget(tp,s.ssfilter,tp,LOCATION_GRAVE,1,1,nil,e,tp,Duel.GetLocationCount(tp,LOCATION_MZONE)>0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
