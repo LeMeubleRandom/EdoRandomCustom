d299dbbdf982f7c0af188717460e5cf2c3804135
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-09 04:29:56 +0200

    correction script 05

--- script/Reclaimer/c955000005.lua
+++ script/Reclaimer/c955000005.lua
@@ -5,4 +5,30 @@ local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
 	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTunerEx(Card.IsSetCard,SET_RECRUIT),1,99)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SET)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetTarget(s.sstg)
+	e1:SetOperation(s.ssop)
+	c:RegisterEffect(e1)
+end
+function s.ssfilter(c,e,mzone_chk)
+    return (c:IsSetCard(SET_RECRUIT) or c:IsLink(1) and c:IsAttack(2000)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and mzone_chk
+end
+function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.ssfilter,tp,LOCATION_GRAVE,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.ssfilter,tp,LOCATION_GRAVE,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+	end
 end
