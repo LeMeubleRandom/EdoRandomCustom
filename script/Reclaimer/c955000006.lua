552aa3815d9cc56285c4ea8f54d0324202eccbd5
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-09 20:35:59 +0200

    correction script 06

--- script/Reclaimer/c955000006.lua
+++ script/Reclaimer/c955000006.lua
@@ -4,5 +4,5 @@ Duel.LoadScript("MeubleConstant.lua")
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    Fusion.AddProcMixN(c,true,true,955000010,aux.FilterBoolFunctionEx(Card.IsRace,RACE_GALAXY))
+    Fusion.AddProcMixN(c,true,true,955000010,1,aux.FilterBoolFunctionEx(Card.IsRace,RACE_GALAXY),1)
 end
