5d634ab8153a79581347d19a07abaa06c371e658
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-09 04:31:21 +0200

    correction script 05

--- script/Reclaimer/c955000005.lua
+++ script/Reclaimer/c955000005.lua
@@ -22,7 +22,7 @@ end
 function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.ssfilter,tp,LOCATION_GRAVE,1,nil,e,tp) end
+		and Duel.IsExistingTarget(s.ssfilter,tp,LOCATION_GRAVE,1,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.ssfilter,tp,LOCATION_GRAVE,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)end
