36577f2ba468c989df0b1c824d1cb4a7a4c79601
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-09 22:44:32 +0200

    change 006 script

--- script/Reclaimer/c955000006.lua
+++ script/Reclaimer/c955000006.lua
@@ -36,9 +36,7 @@ function s.disop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		--Negate its effects
-        Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		if tc:NegateEffects(e:GetHandler()) then
-            Duel.GetControl(tc,tp,RESET_PHASE|PHASE_END,1)
-        end
+		tc:NegateEffects(e:GetHandler())
+        Duel.GetControl(tc,tp,RESET_PHASE|PHASE_END,1)
 	end
 end
