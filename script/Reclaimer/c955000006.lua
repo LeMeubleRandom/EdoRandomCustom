0c2d52f55902c86d29dd00b3d23c2e3b1b6d9976
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-09 21:09:46 +0200

    change 006 script

--- script/Reclaimer/c955000006.lua
+++ script/Reclaimer/c955000006.lua
@@ -5,4 +5,31 @@ local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcMixN(c,true,true,955000010,1,aux.FilterBoolFunctionEx(Card.IsRace,RACE_GALAXY),1)
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+    e1:SetHintTiming(0,TIMING_STANDBY_PHASE|TIMING_MAIN_END|TIMINGS_CHECK_MONSTER_E)
+	c:RegisterEffect(e1)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.disfilter(chkc) and chkc~=c end
+	if chk==0 then return Duel.IsExistingTarget(s.disfilter,tp,0,LOCATION_MZONE,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local g=Duel.SelectTarget(tp,s.disfilter,tp,0,LOCATION_MZONE,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,tp,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		--Negate its effects
+		tc:NegateEffects(e:GetHandler(),RESET_PHASE|PHASE_END)
+	end
 end
