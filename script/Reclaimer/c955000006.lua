7867dd4f3b9a5c060a64b21a6f2b8a481cba2888
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-09 20:33:50 +0200

    correction script 05

--- script/Reclaimer/c955000006.lua
+++ script/Reclaimer/c955000006.lua
@@ -1,6 +1,7 @@
 Duel.LoadScript("MeubleConstant.lua")
 
 --Infested Sanghel "Elites"
+local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
     Fusion.AddProcMixN(c,true,true,955000010,aux.FilterBoolFunctionEx(Card.IsRace,RACE_GALAXY))
