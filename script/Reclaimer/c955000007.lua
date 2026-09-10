4fd425eb13c5d814f76463efb319f7abecb6c00f
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-10 04:12:48 +0200

    change 007 script

--- script/Reclaimer/c955000007.lua
+++ script/Reclaimer/c955000007.lua
@@ -44,7 +44,7 @@ end
 function s.rescon(sg,e,tp,mg)
 	return Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())>0
 		and sg:FilterCount(Card.IsControler,nil,tp)==1
-        and sg:IsExistingMatchingCard(Card.IsCode,nil,995000010)
+        and sg:IsExists(Card.IsCode,1,nil,995000010)
 end
 function s.selfspcon(e,c)
 	if not c then return true end
