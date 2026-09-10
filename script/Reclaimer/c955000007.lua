293f7d3da430aa2e9be1fc2e215a9540c7271a26
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-10 04:47:41 +0200

    change 007 script

--- script/Reclaimer/c955000007.lua
+++ script/Reclaimer/c955000007.lua
@@ -79,6 +79,7 @@ end
 function s.selfspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
+    c:SetMaterial(g)
 	Duel.Release(g,REASON_COST|REASON_MATERIAL)
 end
 function s.regcon(e)
