f008124d42be3de5c0dc6b57b35f820913e45653
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-11 10:43:22 +0200

    change 010 script

--- script/Reclaimer/c955000010.lua
+++ script/Reclaimer/c955000010.lua
@@ -52,6 +52,6 @@ function s.fusop(e,tp,eg,ep,ev,re,r,rp)
         local fusion_params={
             fusfilter=function(c) return c:IsSetCard(SET_INFESTED) end
         }
-        Fusion.SummonEffTG(fusion_params)(e,tp,eg,ep,ev,re,r,rp)
+        Fusion.SummonEffTG(fusion_params)(e,tp,eg,ep,ev,re,r,rp,1)
     end
 end
