e5561dc55efa0e3872f54b99f83c9002e1b28212
LeMeubleRandom <antoinedbc2005@gmail.com>
2026-09-11 02:10:16 +0200

    change 009 script

--- script/Reclaimer/c955000009.lua
+++ script/Reclaimer/c955000009.lua
@@ -16,9 +16,20 @@ function s.initial_effect(c)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+    e2:SetCondition(s.con)
+	e2:SetTarget(s.target)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
 end
 function s.thfilter(c)
 	return c:IsSetCard(SET_RECRUIT) and c:IsAbleToHand()
+end
+function s.obofilter(c,tp,opp)
+    return c:IsOwner(opp) and Duel.GetMZoneCount(tp,c,tp,LOCATION_REASON_CONTROL)>0
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
@@ -34,4 +45,10 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.BreakEffect()
 		Duel.DiscardHand(tp,nil,1,1,REASON_EFFECT|REASON_DISCARD,nil)
 	end
+end
+function s.con(e)
+	return Duel.GetTurnPlayer()~=e:GetHandlerPlayer()
+end
+function s.target(e,c)
+	return Duel.IsExistingMatchingCard(s.obofilter,tp,0,LOCATION_MZONE,1,nil)
 end
